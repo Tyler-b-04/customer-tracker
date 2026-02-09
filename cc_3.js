f0b36e4aa64f919f2bcb43f04eab9e568d0ae51a
Tyler B <bremont@usf.edu>
2026-02-08 19:54:40 -0500

    add new customer with push and remove first with shift

--- cc_3.js
+++ cc_3.js
@@ -1,1 +1,21 @@
 let customers = [
+    {
+    name: "Devon Turn"
+    email: "turn_devon@gmail.com"
+    purchases: "4k monitor", "RGB Keyboard", "Large Mousepad"
+    },
+    {
+    name: "Sebastian Rod"
+    email: "seabass.rod@gmail.com"
+    purchases: "Headset with Mic"
+    },
+    {
+    name: "Tyler Brem"
+    email: "ty_brem@gmail.com"
+    purchases: "Laptop Case", "Small Mousepad"
+    },
+];
+
+console.log("~~~ New Customer List ~~~")
+console.log(customers);
+
