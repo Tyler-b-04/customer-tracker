e67a5d343dc27bba0784ad3b188a23ec16b4f08e
Tyler B <bremont@usf.edu>
2026-02-08 20:14:36 -0500

    display customers with template literals and purchase counts

--- cc_3.js
+++ cc_3.js
@@ -1,18 +1,18 @@
 let customers = [
     {
-    name: "Devon Turn"
-    email: "turn_devon@gmail.com"
-    purchases: "4k monitor", "RGB Keyboard", "Large Mousepad"
+    name: "Devon Turn",
+    email: "turn_devon@gmail.com",
+    purchases: ["4k monitor", "RGB Keyboard", "Large Mousepad"],
     },
     {
-    name: "Sebastian Rod"
-    email: "seabass.rod@gmail.com"
-    purchases: "Headset with Mic"
+    name: "Sebastian Rod",
+    email: "seabass.rod@gmail.com",
+    purchases: ["Headset with Mic"],
     },
     {
-    name: "Tyler Brem"
-    email: "ty_brem@gmail.com"
-    purchases: "Laptop Case", "Small Mousepad"
+    name: "Tyler Brem",
+    email: "ty_brem@gmail.com",
+    purchases: ["Laptop Case", "Small Mousepad"],
     },
 ];
 
@@ -20,11 +20,27 @@ console.log("~~~ New Customer List ~~~")
 console.log(customers);
 
 customers.push({
-    name: "Connor Garry"
-    email: "big_cg@gmail.com"
-    purchases: "RGB Keyboard"
+    name: "Connor Garry",
+    email: "big_cg@gmail.com",
+    purchases: ["RGB Keyboard"],
 });
 console.log("\nAdded new customer (push): Connor Garry");
 
 let removedCustomer = customers.shift();
 console.log(`Removed first customer (shift): ${removedCustomer.name}`);
+
+customers.forEach((c) => {
+    if (c.name = "Sebastian Rod") {
+        c.email = "sebastian.rod@gmail.com";
+    }
+});
+console.log("\nUpdated email for Sebastian Rod");
+
+customers.forEach((c) => {
+    if (c.name = "Tyler Brem") {
+        c.purchases.push("Wireless Mouse");
+    }
+});
+console.log("Added purchase for Tyler Brem: Wireless Mouse");
+
+
